40ec8cf4874dd8e203f80849fb69a3f94dd6f8ca
Evan Robertson <evan.robertson77@gmail.com>
2025-07-23 22:31:23 -0400

    fix(config): Update Vite configuration import for Vitest compatibility

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
