03a76d4c0b0cb12d70a11b3bb5040b8fe5f3b8d0
Evan Robertson <evan.robertson77@gmail.com>
2025-08-04 15:42:27 -0400

    feat(email): Initial integration
    new email module

--- backend/main.rs
+++ backend/main.rs
@@ -6,10 +6,6 @@ mod infrastructure;
 
 use control::services::startup::StartupService;
 use infrastructure::{logging::LoggingManager, websocket::start_metrics_broadcaster};
-use lettre::message::header::ContentType;
-use lettre::message::Mailbox;
-use lettre::transport::smtp::authentication::Credentials;
-use lettre::{Message, SmtpTransport, Transport};
 
 
 #[tokio::main]
@@ -18,31 +14,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     LoggingManager::initialize();
 
     tracing::info!("Starting the Rext Server 🦖");
-
-    println!("Quick test... trying email");
-    let email = Message::builder()
-        .from(Mailbox::new(Some("NoBody".to_owned()), "nobody@domain.tld".parse().unwrap()))
-        .reply_to(Mailbox::new(Some("Yuin".to_owned()), "yuin@domain.tld".parse().unwrap()))
-        .to(Mailbox::new(Some("Hei".to_owned()), "hei@domain.tld".parse().unwrap()))
-        .subject("Happy new year")
-        .header(ContentType::TEXT_PLAIN)
-        .body(String::from("Be happy!"))
-        .unwrap();
-
-    let creds = Credentials::new("smtp_username".to_owned(), "smtp_password".to_owned());
-
-    // Open a remote connection to gmail
-    let mailer = SmtpTransport::relay("smtp.gmail.com")
-        .unwrap()
-        .credentials(creds)
-        .build();
-
-    // Send the email
-    match mailer.send(&email) {
-        Ok(_) => println!("Email sent successfully!"),
-        Err(e) => panic!("Could not send email: {e:?}"),
-    }
-
 
     // Initialize the application
     let db = StartupService::initialize().await?;
